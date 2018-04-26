2b0636da2d0ae6b745eea7cdb73ff702cc63df20
Nico <33714764+ntold@users.noreply.github.com>
2018-04-26 23:14:34 +0200

    first step in the right way

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,11 @@
 <template>
+<v-app>
     <div class="row card-panel">
+      <div class="center col l1"></div>
       <!-- Kostenstelle -->
-      <div class="input-filed col l2">
+      <div class="col l2">
         <p class="center">Kostenstelle</p>
-        <input type="text" v-model="kostenstelle" placeholder="dropdown todo">  
+        <input type="text" v-model="kostenstelle">  
       </div>
       <!-- Operation -->
       <div class="input-filed col l2">
@@ -13,17 +15,61 @@
       <!-- ta tr -->
       <div class="input-filed col l1">
         <p class="center">Std // ta</p> <!-- Hover for more information ONLY NR -->
-        <input type="text" v-model="ta">  
+        <input type="text" class="center" v-model="ta">  
       </div>
       <div class="input-filed col l1">
         <p class="center">Std // tr</p> <!-- Hover for more information ONLY NR-->
-        <input type="text" v-model="tr">  
+        <input type="text" class="center" v-model="tr"> 
       </div>
+      <!-- Kostensatz -->
+      <div class="col l1">
+        <p class="center"> Kostensatz <br>  Fr / Std  </p>
+        <p class="center"> {{ kostensatz }} </p>
+      </div>
+      <div class="col l1">
+        <p class="center"> Bearbeitungskosten <br> Losgrösse </p>
+        <p class="center"> {{ bearbeitungskosten }} </p>
+      </div>
+      <div class="col l1">
+        <p class="center"> Rüstkosten <br> Losgrösse </p>
+        <p class="center"> {{ rüstkosten }} </p>
+      </div>
+      <!-- + -->
+      <br>
+      <a class="btn-floating btn-medium waves-effect waves-light blue" v-on:click="storeFertigung()"><i class="material-icons">add</i></a>
+      <a class="btn-floating btn-medium waves-effect waves-light red" v-if="rows > 1" v-on:click="rows -= 1" ><i class="material-icons">remove</i></a>
     </div>
-    
+
+      <div v-for="message in messages" v-bind:key="message.id">
+        <div class="card-body">
+        
+        <h6 class="card-subtitle mb-2 text-muted" style="display:inline-block;" >{{ fertigung.message }} </h6>
+        
+        </div>
+      </div>
+</v-app>
 </template>
 
 <script>
+import firebase from 'firebase'
+  
+const config = {
+    apiKey: "AIzaSyBIaj6PnxqwhhOqHP6VF3ql4_CbNF9mOSw",
+    authDomain: "kalkulation-vue.firebaseapp.com",
+    databaseURL: "https://kalkulation-vue.firebaseio.com",
+    projectId: "kalkulation-vue",
+    storageBucket: "kalkulation-vue.appspot.com",
+    messagingSenderId: "411750576448"
+  };
+  firebase.initializeApp(config);
+
+  const database = firebase.database()
+  const FertigungRef = database.ref('fertigung')
+
+
+
+
+
 export default {
   name: 'fertigung',
   data () {
@@ -31,11 +77,36 @@ export default {
       kostenstelle: '',
       operation: '',
       ta: '',
-      tr: ''
+      tr: '',
+      kostensatz: '',
+      bearbeitungskosten: '',
+      rüstkosten: '',
+      rows: 1,
+      messages: [],
+
     }
-  }
+  },
+  methods: {
+    storeFertigung(){
+      console.log("hi")
+      FertigungRef.push({kostenstelle: this.kostenstelle, operation: this.operation})
+      this.kostenstelle = ''
+      this.operation = ''
+      this.addRow()
+    },
+    addRow(){
+      this.rows += 1
+      return this.rows
+    },
+
+
+  },
+  created: {}
 
 }
+
+// Autocomplete
+//  :error-messages="['Please select an option']"
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.min.css' 
+
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
