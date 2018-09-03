a184a541c7a88584b4c756ac382a5b3eb8f6d860
Dany Lunt <33714789+D4nyy@users.noreply.github.com>
2018-09-03 12:05:38 +0200

    Overlay "Vorhandene Auswählen"🛫

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="container">
     <div class="center">
-        <h4 class="truncate">Willkommen zurück</h4>
+      <h4 class="truncate">Willkommen zurück</h4>
     </div>
     <div class="push"></div>
     <div class="row">
 		<div class="col l6">
 			<a class="waves-effect waves-light btn-large right btn1" v-on:click="fertigungActive = true">Neue Fertigung</a>
 		</div>
-    <div v-if="fertigungActive" id="modal-template" type="text/x-template"> 
+    <div v-if="fertigungActive"> 
       <transition name="modal">
         <div class="modal-mask">
           <div class="modal-wrapper">
@@ -18,14 +18,12 @@
                 <div class="modal-header">Neue Fertigung</div>
                 <div></div>
               </div>
-              
               <div class="modal-body">
                 <slot name="body">
                   <input type="text" class="left" placeholder="Name">
                   <input type="text" class="left" placeholder="Ersteller">
                 </slot>
               </div>
-
               <div class="modal-footer">
                 <slot class="footer-grid" name="footer">
                   <button class="date" disabled> {{datum}} </button>
@@ -40,8 +38,53 @@
       </transition>
     </div>
 		<div class="col l6">
-			<a class="waves-effect waves-light btn-large btn2">Vorhandene auswählen</a>
+			<a class="waves-effect waves-light btn-large btn2" v-on:click="existingActive = true">Vorhandene auswählen</a>
 		</div>
+    <div v-if="existingActive"> 
+      <transition name="modal">
+        <div class="modal-mask">
+          <div class="modal-wrapper">
+            <div class="modal-container2">
+              <div class="header-grid">
+                <div class="cancel" v-on:click="existingActive = false"><i class="material-icons">close</i></div>
+                <div class="modal-header">Fertigungen</div>
+                <div></div>
+              </div>
+              
+              <div class="modal-body">
+                <slot name="body">
+                    <form class="example-form">
+                    <mat-form-field class="example-full-width">
+                      <mat-autocomplete autoActiveFirstOption >
+                        <mat-option>
+                          <div class="card-panel card-box">
+                            <div class="thead-grid">
+                              <div>Name / Ersteller</div>
+                              <div>Status / Bearbeitet</div>
+                            </div>
+                            <div class="calc-row" v-for="(dataRow, index) in dataRows" :key="index">
+                              <div class="name">{{dataRow.Name}}</div>
+                              <div class="status">{{dataRow.Status}}</div>
+                              <div class="icon-wrap"><i class="material-icons">delete</i></div>
+                              <div class="creator">{{dataRow.Creator}}</div>
+                              <div class="updated">{{dataRow.Updated}}</div>
+                            </div>
+                          </div>
+                        </mat-option>
+                      </mat-autocomplete>
+                    </mat-form-field>
+                  </form>
+                </slot>
+              </div>
+              <div class="modal-footer">
+                <slot class="footer-grid" name="footer">
+                </slot>
+              </div>
+            </div>
+          </div>
+        </div>
+      </transition>
+    </div>
     </div>
   </div>
 </template>
@@ -51,12 +94,31 @@ export default {
   name: "Home",
   data() {
     return {
-      fertigungActive: false
+      fertigungActive: false,
+      existingActive: false,
+      dataRows: [
+                  { 
+                   'Name' : 'Herrschaftsspiel', 
+                   'Creator' : 'Dany', 
+                   'Status' : 'In Bearbeitung', 
+                   'Updated' : '03.09.2018'
+                   },
+                   { 
+                   'Name' : 'Erarbeitetes Bruttoeinkommen', 
+                   'Creator' : 'Dany', 
+                   'Status' : 'In Bearbeitung', 
+                   'Updated' : '03.09.2018'
+                   },
+                ],
+
     };
   },
   methods: {
     neueFertigung() {
       this.fertigungActive = true;
+    },
+    existing() {
+      this.existingActive = true;
     }
   },
   computed: {
@@ -75,8 +137,7 @@ export default {
       month[9] = "Oktober";
       month[10] = "November";
       month[11] = "Dezember";
-      this.time =
-        d.getDate() + ". " + month[d.getMonth()] + " " + d.getFullYear();
+      this.time = (d.getDate() + ". " + month[d.getMonth()] + " " + d.getFullYear());
       return this.time;
     }
   }
@@ -84,6 +145,9 @@ export default {
 </script>
 
 <style scoped>
+
+/*Buttons*/
+
 .push {
   margin-bottom: 70px;
 }
@@ -105,6 +169,8 @@ export default {
   background-color: #005b7d;
 }
 
+/*Overlay Neue & Vorhandene*/
+
 .modal-mask {
   position: fixed;
   z-index: 9998;
@@ -112,9 +178,9 @@ export default {
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
+  background-color: rgba(0, 0, 0, .5);
   display: table;
-  transition: opacity 0.3s ease;
+  transition: opacity .3s ease;
 }
 
 .modal-wrapper {
@@ -122,19 +188,7 @@ export default {
   vertical-align: middle;
 }
 
-.modal-container {
-  width: 400px;
-  height: 300px;
-  margin: 0 auto;
-  padding: 20px 30px;
-  background-color: #fff;
-  border-radius: 2px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
-  transition: all 0.3s ease;
-  font-family: Helvetica, Arial, sans-serif;
-}
-
-.modal-header {
+.modal-header{
   font-size: 20px;
   margin-top: 0;
   text-align: center;
@@ -148,12 +202,11 @@ export default {
   float: right;
 }
 
-.modal-enter,
-.modal-leave-active {
+.modal-enter, .modal-leave-active {
   opacity: 0;
 }
 
-.header-grid {
+.header-grid{
   display: grid;
   grid-template-columns: 0.05fr 1fr 0.05fr;
 }
@@ -163,29 +216,16 @@ export default {
   color: red;
   font-size: 20px;
   display: flex;
-  display: flex;
   align-items: center;
   justify-content: center;
-  -webkit-transition: -webkit-transform 0.25s, opacity 0.25s;
-  -moz-transition: -moz-transform 0.25s, opacity 0.25s;
-  transition: transform 0.25s, opacity 0.25s;
 }
-
 .cancel:hover {
   color: firebrick;
-  -webkit-transform: rotate(180deg);
-  -moz-transform: rotate(180deg);
-  transform: rotate(180deg);
-  opacity: 1;
 }
 
-.btn-ok {
+.btn-ok{
   width: 140px !important;
   float: right;
-  margin-top: 50px;
-}
-
-.date {
   margin-top: 50px;
 }
 
@@ -195,8 +235,105 @@ export default {
   transform: scale(1.1);
 }
 
-.footer-grid {
+/*Neue Fertigung*/
+
+.modal-container {
+  width: 400px;
+  height: 300px;
+  margin: 0 auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
+  transition: all .3s ease;
+  font-family: Helvetica, Arial, sans-serif;
+}
+
+.date{
+  margin-top: 50px;
+}
+
+.footer-grid{
   display: grid;
   grid-template-columns: 1fr 1fr;
 }
+
+/*Vorhadene Auswählen*/
+
+.modal-container2{
+  width: 800px;
+  height: 600px;
+  margin: 0 auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
+  transition: all .3s ease;
+  font-family: Helvetica, Arial, sans-serif;
+}
+
+.card-box{
+  overflow:auto; 
+  height:500px;
+}
+
+.sticky {
+  position: fixed;
+  top: 0;
+  width: 100%;
+}
+
+.thead-grid{
+  display: grid;
+  grid-template-columns: 1fr 250px 50px;
+
+  background-color: #bbb;
+  padding: 5px;
+}
+
+.calc-row{
+  display: grid;
+  grid-template-columns: 1fr 250px 50px;
+  grid-template-rows: 1fr 1fr;
+
+  padding: 5px;
+}
+
+.calc-row:nth-child(odd){
+  background-color: #eee;
+}
+
+.calc-row:hover{
+  cursor: pointer;
+  background-color: #0082b4;
+  transition: ease .1s;
+  color: white;
+
+  -webkit-box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);
+     -moz-box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);
+          box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);
+}
+
+.calc-row .name{
+  margin-left: 0em;   
+  font-weight: bold;
+  display:inline-block;
+  white-space: nowrap;
+  overflow: hidden;
+
+}
+
+.calc-row .creator{
+  font-size: 12px;
+}
+
+.material-icons:hover{
+  color: firebrick;
+}
+
+.material-icons{
+  position: absolute;
+  margin-top: 8.5px;
+}
+
 </style>
