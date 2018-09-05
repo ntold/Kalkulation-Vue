9bf4611f8bdc4383a582cd3e5c45b73ad83f7471
Mac <33714767+McKensy@users.noreply.github.com>
2018-09-05 16:49:10 +0200

    Extended Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,20 +1,37 @@
 <template>
-  <nav>
-      <div class="nav-wrapper #424242 color">
+  <nav class="nav-extended color">
+      <div class="nav-wrapper colored">
           <div class="row">
-            <div class="col s1 m1 l1"></div>
+            <div class="col m1 l1"></div>
             <router-link to="/" class="brand-logo left"  style="text-decoration:none;"> TFBern </router-link>
             <router-link to="/" class="brand-logo center"  style="text-decoration:none;"> Kalkulation </router-link>
           </div>
       </div>
+      <div class="nav-content color">
+        <div class="row center">
+          <div class="center">
+            <div class="col m1"></div>
+              <div class="col m10">
+                <ul class="tabs tabs-transparent">
+                  <li class="tab"><router-link to="/" tag="a">Home</router-link></li>
+                  <li class="tab"><router-link to="/fertigung" tag="a">Fertigung</router-link></li>
+                  <li class="tab"><router-link to="/material" tag="a">Material</router-link></li>
+                  <li class="tab"><router-link to="/oberflaeche" tag="a">Oberfläche</router-link></li>
+                  <li class="tab"><router-link to="/werkzeug" tag="a">Werkzeug</router-link></li>
+                </ul>
+              </div>
+            <div class="col m1"></div>
+          </div>
+        </div>
+      </div>
   </nav>
+
 </template>
 
 <script>
 export default {
-  name: "navbar",
   data() {
-    return {};
+    return { }
   }
 };
 </script>
