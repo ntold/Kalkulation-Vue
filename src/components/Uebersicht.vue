44d4b5b63d56c39dce76207d518d473d2c4fca89
Mac <33714767+McKensy@users.noreply.github.com>
2018-09-07 14:47:51 +0200

    Added 'Uebersicht.vue'

---
+++ src/components/Uebersicht.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-app>
+    <div class="container">
+      <h1>Kalkulationsblatt</h1>
+    </div>
+  </v-app>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -8,17 +8,9 @@ import Material from '@/components/Material'
 import Werkzeug from '@/components/Werkzeug'
 import Oberflaeche from '@/components/Oberflaeche'
 import Einmaligekosten from '@/components/Einmaligekosten'
+import Uebersicht from '@/components/Uebersicht'
 
 Vue.use(Router)
-
-const User = {
-  template: `
-    <div class="user">
-      <h2>User {{ $route.params.id }}</h2>
-      <router-view></router-view>
-    </div>
-  `
-}
 
 export default new Router({
   mode: 'history',
@@ -53,6 +45,11 @@ export default new Router({
           name: 'Einmaligekosten',
           path: 'einmaligekosten',
           component: Einmaligekosten
+        },
+        {
+          name: 'Uebersicht',
+          path: 'uebersicht',
+          component: Uebersicht
         }
       ]
     }
