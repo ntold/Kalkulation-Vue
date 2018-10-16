e0be37bd0207334aec47a53053570b896c283305
Dany <danyyil.luntovsky@gmail.com>
2018-10-16 19:42:46 +0200

    Total.vue button fix ✔

--- src/components/Total.vue
+++ src/components/Total.vue
@@ -1,80 +1,82 @@
 <template>
-    <div class="content">
-        <div class="content-wrap">
-            <div class="total-title">
-                <div class="title-head">Bezeichnung</div>
-                <div class="title-head">Zeichen Nr.</div>
-                <div class="title-head">Losgrösse</div>
-                <div class="title-head">Datum</div>
-                <div class="title-head">Visum</div>
-                <div class="title-item">JS_INPUT</div>
-                <div class="title-item">JS_INPUT</div>
-                <div class="title-item">JS_INPUT</div>
-                <div class="title-item">JS_INPUT</div>
-                <div class="title-item">JS_INPUT</div>
-            </div>
-            <div class="content-page">
-                <div>
-                    <!-- Title -->
-                    <div>Fertigungslohnkosten</div>
-                    <div>Fertigungsgemeinkosten ( 10% )</div>
-                    <div>Fertigungskosten</div>
+    <v-app>
+        <div class="content">
+            <div class="content-wrap">
+                <div class="total-title">
+                    <div class="title-head">Bezeichnung</div>
+                    <div class="title-head">Zeichen Nr.</div>
+                    <div class="title-head">Losgrösse</div>
+                    <div class="title-head">Datum</div>
+                    <div class="title-head">Visum</div>
+                    <div class="title-item">JS_INPUT</div>
+                    <div class="title-item">JS_INPUT</div>
+                    <div class="title-item">JS_INPUT</div>
+                    <div class="title-item">JS_INPUT</div>
+                    <div class="title-item">JS_INPUT</div>
                 </div>
-                <div>
+                <div class="content-page">
+                    <div>
+                        <!-- Title -->
+                        <div>Fertigungslohnkosten</div>
+                        <div>Fertigungsgemeinkosten ( 10% )</div>
+                        <div>Fertigungskosten</div>
+                    </div>
+                    <div>
+                        <!-- Values -->
+                        <div class="content-page-value">JS_INPUT</div>
+                        <div class="content-page-value">JS_INPUT</div>
+                        
+                    </div>
+
+                    <div class="content-page-value">JS_INPUT</div>
+
+                    <div>
+                        <!-- Title -->
+                        <div>Materialeinzelkosten</div>
+                        <div>Materialgemainkosten</div>
+                        <div>Materialkosten</div>
+                    </div>
+                    <div>
+                        <!-- Values -->
+                        <div class="content-page-value">JS_INPUT</div>
+                        <div class="content-page-value">JS_INPUT</div>
+                        <div class="content-page-value">JS_INPUT</div>
+                    </div>
+
+                    <!-- Title -->
+                    <div>Sondereinzelkosten</div>
                     <!-- Values -->
                     <div class="content-page-value">JS_INPUT</div>
-                    <div class="content-page-value">JS_INPUT</div>
-                    
-                </div>
 
-                <div class="content-page-value">JS_INPUT</div>
-
-                <div>
                     <!-- Title -->
-                    <div>Materialeinzelkosten</div>
-                    <div>Materialgemainkosten</div>
-                    <div>Materialkosten</div>
-                </div>
-                <div>
+                    <div>Herstellungskosten</div>
                     <!-- Values -->
                     <div class="content-page-value">JS_INPUT</div>
+
+                    <!-- Title -->
+                    <div>VVGK ( 5% v. HK )</div>
+                    <!-- Values -->
                     <div class="content-page-value">JS_INPUT</div>
+
+                    <!-- Title -->
+                    <div>Selbstkosten</div>
+                    <!-- Values -->
                     <div class="content-page-value">JS_INPUT</div>
                 </div>
-
-                <!-- Title -->
-                <div>Sondereinzelkosten</div>
-                <!-- Values -->
-                <div class="content-page-value">JS_INPUT</div>
-
-                <!-- Title -->
-                <div>Herstellungskosten</div>
-                <!-- Values -->
-                <div class="content-page-value">JS_INPUT</div>
-
-                <!-- Title -->
-                <div>VVGK ( 5% v. HK )</div>
-                <!-- Values -->
-                <div class="content-page-value">JS_INPUT</div>
-
-                <!-- Title -->
-                <div>Selbstkosten</div>
-                <!-- Values -->
-                <div class="content-page-value">JS_INPUT</div>
             </div>
-        </div>
-        <div class="fixed-action-btn" v-on:click="goTo()">
-          <button class="btn right btnnext">Weiter
-            <i class="material-icons right">arrow_forward_ios</i>
-          </button>
-        </div>
-        <div class="fixed-action-btn goleft" v-on:click="goBack()">
-            <button class="btn right btnnext">
-            <i class="material-icons left">arrow_back_ios</i>
-            Zurück
+            <div class="fixed-action-btn" v-on:click="goTo()">
+            <button class="btn right btnnext">Weiter
+                <i class="material-icons right">arrow_forward_ios</i>
             </button>
+            </div>
+            <div class="fixed-action-btn goleft" v-on:click="goBack()">
+                <button class="btn left btnnext">
+                <i class="material-icons left">arrow_back_ios</i>
+                Zurück
+                </button>
+            </div>
         </div>
-    </div>
+    </v-app>
 </template>
 
 <script>
@@ -130,9 +132,9 @@ export default {
     .content-page-value{
         text-align: center;
     }
-    .btn, .btn:hover, .btn:visited{
-        color: black;
-        background-color: rgb(250, 250, 250);
+
+    .goleft {
+        margin-right: 87%;
     }
 }
 
